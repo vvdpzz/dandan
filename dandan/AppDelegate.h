98cc1e5fea5fa4f196c588443ee300b6f15089ff
vvdpzz <vvdpzz@gmail.com>
2012-05-26 10:46:58 +0800

    Initial Commit

---
+++ dandan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dandan
+//
+//  Created by  on 12-5-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dandan/SecondViewController.m
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.m
+//  dandan
+//
+//  Created by  on 12-5-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
