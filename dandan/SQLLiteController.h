25d0b4b08a397121efbe07a9981b7b85c1ef0346
xxd <xxdxxd51@hotmail.com>
2012-06-09 13:39:28 +0800

    create sqllite

---
+++ dandan/SQLLiteController.h
@@ -0,0 +1,13 @@
+//
+//  SQLLiteController.h
+//  dandan
+//
+//  Created by xiaodong xue on 12-6-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SQLLiteController : NSObject
+
+@end
---
+++ dandan/SQLLiteController.m
@@ -0,0 +1,13 @@
+//
+//  SQLLiteController.m
+//  dandan
+//
+//  Created by xiaodong xue on 12-6-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SQLLiteController.h"
+
+@implementation SQLLiteController
+
+@end
